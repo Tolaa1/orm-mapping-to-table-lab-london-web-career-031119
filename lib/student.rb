128c1763c72b42d192507575eed49718b71581f0
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-25 17:59:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,8 +86,8 @@ def save
 
 end
 
-def self.create(id=nil name:, grade:)
-  student = Student.new(name, grade)
+def self.create(id=nil, name:, grade:)
+  student = Student.new(id=nil,name, grade)
   student.save
   student
 end
